1512ec824b0f6d5591e5a47c773512d9a92ee1d8
Sohel <sohailmaniyar99@gmail.com>
2024-03-27 00:07:07 +0530

    circuit breaker implemented

--- userservice/src/main/java/com/lcwd/user/service/userservice/controllers/UserController.java
+++ userservice/src/main/java/com/lcwd/user/service/userservice/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.lcwd.user.service.userservice.controllers;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import org.apache.logging.log4j.spi.LoggerContextFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lcwd.user.service.userservice.entities.User;
 import com.lcwd.user.service.userservice.services.UserService;
 
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    private Logger logger=Logger.getAnonymousLogger();
     
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
@@ -29,10 +37,19 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUser(@PathVariable String userId){
         User user=userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
+
+    //creating fallback method for circuit breaker
+    public ResponseEntity<User> ratingHotelFallback(@PathVariable String userId,Exception ex){
+        logger.info("Fallback is excuted because service is down : "+ex.getMessage());
+        User user=User.builder().email("dummay@gmail.com").name("Dummy").about("This user is created dummy bcs some service r down").userId("1111111").build();
+        return new ResponseEntity<User>(user,HttpStatus.OK);
+    }
+
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUser(){
